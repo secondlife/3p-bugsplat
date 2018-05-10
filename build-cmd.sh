c8fa4b0b5a9762c65232488304e738414c26604f
Nat Goodspeed <nat@lindenlab.com>
2018-05-10 12:11:56 -0400

    SL-821: Copy prebuilt BugsplatMac.framework to stage/lib/release and package it in the Mac autobuild package tarball.

--- build-cmd.sh
+++ build-cmd.sh
@@ -31,50 +31,54 @@ source_environment_tempfile="$stage/source_environment.sh"
 "$autobuild" source_environment > "$source_environment_tempfile"
 . "$source_environment_tempfile"
 
-# unable to locate version info in header file or a method to
-# access it so, for now, hard coding it based on observation of
-# this page: https://www.bugsplat.com/docs/resources/updates
-echo "3.5.0.5" > "$stage/version.txt"
-
 build=${AUTOBUILD_BUILD_ID:=0}
 
+# prepare the staging dirs
+mkdir -p "$stage/LICENSES"
+mkdir -p "$stage/include/bugsplat"
+mkdir -p "$stage/lib/release"
+
 case "$AUTOBUILD_PLATFORM" in
-    windows)
+    windows*)
         load_vsvars
 
-        # prepare the staging dirs
-        mkdir -p "$stage/LICENSES"
-        mkdir -p "$stage/include/bugsplat"
-        mkdir -p "$stage/lib/release"
+        if [ "$AUTOBUILD_PLATFORM" == "windows64" ]
+        then
+            lib="lib64"
+            bin="bin64"
+            sfx="64"
+            rcdll="BugSplatRc64.dll"
+        else
+            lib="lib"
+            bin="bin"
+            sfx=""
+            rcdll="BugSplatRC.dll"
+        fi
+
+        # BugSplat version info seems to be platform-dependent and even
+        # component-dependent?! Query the Windows version by asking for the
+        # version stamped into this BugSplat .exe.
+        BUGSPLAT_VERSION="$("$top/query_version.sh" "$BUGSPLAT_DIR/bin/BsSndRpt.exe")"
 
         # copy files
-        cp "$BUGSPLAT_DIR/BUGSPLAT_LICENSE.txt" "$stage/LICENSES"
         cp "$BUGSPLAT_DIR/inc/BugSplat.h" "$stage/include/bugsplat"
-        cp "$BUGSPLAT_DIR/lib/BugSplat.lib" "$stage/lib/release"
-        cp "$BUGSPLAT_DIR/bin/BsSndRpt.exe" "$stage/lib/release"
-        cp "$BUGSPLAT_DIR/bin/BugSplat.dll" "$stage/lib/release"
-        cp "$BUGSPLAT_DIR/bin/BugSplatRC.dll" "$stage/lib/release"
-    ;;
-    windows64)
-        load_vsvars
-
-        # prepare the staging dirs
-        mkdir -p "$stage/LICENSES"
-        mkdir -p "$stage/include/bugsplat"
-        mkdir -p "$stage/lib/release"
-
-        # copy files
-        cp "$BUGSPLAT_DIR/BUGSPLAT_LICENSE.txt" "$stage/LICENSES"
-        cp "$BUGSPLAT_DIR/inc/BugSplat.h" "$stage/include/bugsplat"
-        cp "$BUGSPLAT_DIR/lib64/BugSplat64.lib" "$stage/lib/release/BugSplat.lib"  # rename since we can't branch on 32/64 in CMake files
-        cp "$BUGSPLAT_DIR/bin64/BsSndRpt64.exe" "$stage/lib/release"
-        cp "$BUGSPLAT_DIR/bin64/BugSplat64.dll" "$stage/lib/release"
-        cp "$BUGSPLAT_DIR/bin64/BugSplatRc64.dll" "$stage/lib/release"
+        # force to simple name since we can't branch on 32/64 in CMake files
+        cp "$BUGSPLAT_DIR/$lib/BugSplat$sfx.lib" "$stage/lib/release/BugSplat.lib"
+        cp "$BUGSPLAT_DIR/$bin/BsSndRpt$sfx.exe" "$stage/lib/release"
+        cp "$BUGSPLAT_DIR/$bin/BugSplat$sfx.dll" "$stage/lib/release"
+        cp "$BUGSPLAT_DIR/$bin/$rcdll" "$stage/lib/release"
     ;;
     darwin*)
-        echo "This project is not currently supported for $AUTOBUILD_PLATFORM" 1>&2 ; exit 1
+        # BugsplatMac version embedded in the framework's Info.plist
+        framework="$top/Carthage/Build/Mac/BugsplatMac.framework"
+        Info_plist="$framework/Resources/Info.plist"
+        BUGSPLAT_VERSION="$(python -c "import plistlib
+print plistlib.readPlist('$Info_plist')['CFBundleShortVersionString']")"
+        cp -R "$framework" "$stage/lib/release"
     ;;
     linux*)
         echo "This project is not currently supported for $AUTOBUILD_PLATFORM" 1>&2 ; exit 1
     ;;
 esac
+echo "$BUGSPLAT_VERSION.$build" > "$stage/version.txt"
+cp "$BUGSPLAT_DIR/BUGSPLAT_LICENSE.txt" "$stage/LICENSES"
