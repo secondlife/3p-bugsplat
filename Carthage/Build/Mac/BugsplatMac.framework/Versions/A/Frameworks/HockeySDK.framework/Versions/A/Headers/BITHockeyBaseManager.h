8c1a670068315ba0a29f541541c8441f6ceb7ad3
Nat Goodspeed <nat@lindenlab.com>
2018-08-23 10:22:09 -0400

    DRTVWR-447: Update to vendor drop BugsplatMac 1.0.6.
    --HG--
    branch : vendor

--- Carthage/Build/Mac/BugsplatMac.framework/Versions/A/Frameworks/HockeySDK.framework/Versions/A/Headers/BITHockeyBaseManager.h
+++ Carthage/Build/Mac/BugsplatMac.framework/Versions/A/Frameworks/HockeySDK.framework/Versions/A/Headers/BITHockeyBaseManager.h
@@ -21,6 +21,16 @@
 @property (nonatomic, copy) NSString *serverURL;
 
 /**
+ *  Represents user's full name
+ */
+@property (nonatomic, copy) NSString *userName;
+
+/**
+ *  Represents user's email address
+ */
+@property (nonatomic, copy) NSString *userEmail;
+
+/**
  *  Flag to enable/disable storing user settings in keychain
  */
 @property (nonatomic, assign) BOOL persistUserInfo;
